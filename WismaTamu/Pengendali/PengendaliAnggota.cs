77f60f5c01839e96b5f8c51f9a9d5ca2335fa516
Tini <kartini.103@gmail.com>
2013-06-12 00:47:06 +0700

    Pengendali/PengendaliAnggota.cs oleh fitri Pengendali/PengendaliKamar.cs oleh kartini

---
+++ WismaTamu/Pengendali/PengendaliAnggota.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WismaTamu.Model;
+
+namespace WismaTamu.Pengendali
+{
+    public class PengendaliAnggota
+    {
+        private static WismaTamuDb db = new WismaTamuDb();
+        public static List<Anggota> LihatSemuaAnggota(string idAnggota)
+        {
+            return (db.Anggota.Where(x => x.IdAnggota == idAnggota)).ToList();
+        }
+
+        public static void SuntingDataAnggota(Anggota dataBaru, string idAnggota)
+        {
+            try
+            {
+                var dataAnggota = db.Anggota.SingleOrDefault(x => x.IdAnggota == idAnggota);
+                dataAnggota = dataBaru;
+                db.SaveChanges();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static void TambahDataAnggota(Anggota dataBaru)
+        {
+            db.Anggota.Add(dataBaru);
+            db.SaveChanges();
+        }
+    }
+}
