d88b649ee5784bb6ed00a7b6d18eb7e3c1d7d853
Tini <kartini.103@gmail.com>
2013-06-11 20:56:45 +0700

    Diimplementasikan kelas Anggota (titipan dari fitriharya, kelas Kamar (dari saya sendiri) dan kelas Pesanan (titipan dari indrasaputra)

---
+++ WismaTamu/Model/Anggota.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WismaTamu.Model
+{
+    public class Anggota
+    {
+        [Required(ErrorMessage = "Id Anggota belum ditentukan")]
+        [Key]
+        public string IdAnggota { get; set; }
+        
+        [Required(ErrorMessage = "Alamat Anggota belum ditentukan")]
+        public string AlamatAnggota { get; set; }
+        
+        [Required(ErrorMessage = "Nama Anggota belum ditentukan")]
+        public string NamaAnggota { get; set; }
+        
+        [Required(ErrorMessage = "Nomor Kontak Anggota belum ditentukan")]
+        public string NomorKontakAnggota { get; set; }
+        
+        [Required(ErrorMessage = "Password Anggota belum ditentukan")]
+        public string PasswordAnggota { get; set; }
+        
+        [Required(ErrorMessage = "Alamat Surel Anggota belum ditentukan")]
+        public string SurelAnggota { get; set; }
+    }
+}
